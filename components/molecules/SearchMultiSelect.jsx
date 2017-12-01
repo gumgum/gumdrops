8748ebc97d36c555a339fc7f33a503302fb849f8
michele <michele@gumgum.com>
2017-12-01 13:30:42 -0800

    fix arraysEqual import

--- components/molecules/SearchMultiSelect.jsx
+++ components/molecules/SearchMultiSelect.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import arraysEqual from '../../utils/arraysEqual';
+import arraysEqual from '../utils/arraysEqual';
 
 class SearchMultiSelect extends React.Component {
 
