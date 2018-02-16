412b4e378d5868a7acc7606bc0604f3b29961f79
Eder Sanchez <eder@gumgum.com>
2018-02-16 15:41:22 -0600

    Fix issue where close button is recognized as submit

--- components/atoms/ModalHeader.jsx
+++ components/atoms/ModalHeader.jsx
@@ -4,7 +4,9 @@ import PropTypes from 'prop-types';
 const ModalHeader = ({ title, onClose, className, style }) => (
     <div className={`gds-modal__header ${className}`} style={style}>
         {title && <h2 className="gds-modal__title gds-text--header-sm">{title}</h2>}
-        {onClose && <button onClick={onClose} className="gds-modal__close-button -z-9" />}
+        {onClose && (
+            <button onClick={onClose} type="button" className="gds-modal__close-button -z-9" />
+        )}
     </div>
 );
 
