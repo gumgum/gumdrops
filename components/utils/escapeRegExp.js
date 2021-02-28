9a6b1dd41c275b47200ea0986d6afa18c478cc69
Eder Sanchez <eder@gumgum.com>
2021-02-27 22:56:09 -0600

    you're still holding on
    let go of the past

--- components/utils/escapeRegExp.js
+++
@@ -1,8 +0,0 @@
-/**
- * Escape special characters for RegEx
- * @param   {String}    term
- * @returns {Boolean}
- */
-const escapeRegExp = term => term.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-
-export default escapeRegExp;
