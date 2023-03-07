878be943961e5129eab5a7d5f4bbd19fc5814fee
Michele Larson <michele@gumgum.com>
2023-03-07 07:56:12 -0800

    106 update storybook and make first component compatible

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,20 @@
 module.exports = {
-    stories: ['../_stories/**/*.stories.@(js|mdx)'],
-    addons: [
-        '@storybook/addon-knobs/register',
-        {
-            name: '@storybook/addon-docs',
-            options: {
-                configureJSX: true,
-                babelOptions: {},
-                sourceLoaderOptions: null
-            }
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        sass: {
+          // Require your Sass preprocessor here
+          implementation: require('sass'),
         },
-        '@storybook/addon-options/register',
-        '@storybook/addon-actions/register'
-    ]
-};
+      },
+    },
+  ],
+  "framework": "@storybook/react"
+}
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,50 +1,13 @@
-import React from 'react';
-import { configure, addDecorator, addParameters } from '@storybook/react';
-import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
-import { withInfo } from '@storybook/addon-info';
-import { create } from '@storybook/theming';
-
 import './index.scss';
 
-addParameters({
-    docs: {
-        container: DocsContainer,
-        page: DocsPage
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
     },
-    options: {
-        isFullScreen: false,
-        showNav: true,
-        showPanel: true,
-        panelPosition: 'right',
-        sortStoriesByKind: true,
-        hierarchySeparator: /\/|\./,
-        hierarchyRootSeparator: /\|/,
-        sidebarAnimations: false,
-        enableShortcuts: true,
-        isToolshown: true,
-        theme: create({
-            base: 'light',
-            brandTitle: 'Gumdrops',
-            brandUrl: 'https://gumdrops.gumgum.com'
-        })
-    }
-});
-
-function loadStories() {
-    require('../_stories/');
+  },
 }
 
-const storyWrapper = story => <div style={{ margin: 35 }}>{story()}</div>;
-
-addDecorator(
-    withInfo({
-        inline: true,
-        header: false,
-        source: true,
-        propTables: false
-    })
-);
-
-addDecorator(storyWrapper);
-
-configure(loadStories, module);
+export const argTypes = { className: '', style: {}};
